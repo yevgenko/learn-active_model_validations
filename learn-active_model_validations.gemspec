371bfc47b43d0404e0f35d01bbb3e56fcc5426bd
Yevhen V <wik@bdhr.co>
2019-05-02 23:48:25 +0100

    Know how to use active model validations

--- learn-active_model_validations.gemspec
+++ learn-active_model_validations.gemspec
@@ -36,4 +36,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  spec.add_development_dependency "activemodel", "~> 5.2"
 end
--- spec/learn/active_model_validations_spec.rb
+++ spec/learn/active_model_validations_spec.rb
@@ -1,9 +1,33 @@
+require 'active_model'
+
 RSpec.describe Learn::ActiveModelValidations do
-  it "has a version number" do
-    expect(Learn::ActiveModelValidations::VERSION).not_to be nil
+  let(:person_class) do
+    Struct.new(:first_name, :last_name) do
+      include ActiveModel::Validations
+
+      validates_each :first_name, :last_name do |record, attr, value|
+        record.errors.add attr, 'starts with z.' if value.to_s[0] == ?z
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "verifies validity of the entity" do
+    expect(person_class.new).to               be_valid
+    expect(person_class.new('aaa', 'aaa')).to be_valid
+
+    expect(person_class.new('zzz')).to        be_invalid
+    expect(person_class.new('aaa', 'zzz')).to be_invalid
+  end
+
+  it 'reports errors messages' do
+    invalid_person = person_class.new('zzz')
+
+    expect{
+      invalid_person.valid?
+    }.to change{
+      invalid_person.errors.messages
+    }.from({}).to(
+      { first_name: ["starts with z."] }
+    )
   end
 end
